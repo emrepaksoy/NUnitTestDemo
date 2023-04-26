e974b899b1111d26bd8a6873f3d0bb55f5f49183
Emre Paksoy <emre.paksoy@medyasoft.com.tr>
2023-04-26 10:38:45 +0300

    farklı test senaryoları

--- NUnitDemo.JobApplicaton.Test/ApplicationEvaluateUnitTest.cs
+++ NUnitDemo.JobApplicaton.Test/ApplicationEvaluateUnitTest.cs
@@ -1,6 +1,6 @@
 using NUnitTestDemo;
 using NUnitTestDemo.Models;
-
+using FluentAssertions;
 namespace NUnitDemo.JobApplicaton.Test
 {
     public class ApplicationEvaluateUnitTest
@@ -22,7 +22,60 @@ namespace NUnitDemo.JobApplicaton.Test
             var appResult =  evaluator.Evaluate(form);
 
             //Assert
+            //Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
+
+            //FluentAssertion
+            appResult.Should().Be(ApplicationResult.AutoRejected);
+        }
+
+        [Test]
+        public void Application_SouldTransferredToAutoRejected_WithNoTechStack()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant()
+                {
+                    Age = 19
+                },
+                TechStackList = new System.Collections.Generic.List<string>() {""}
+                
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
             Assert.AreEqual(ApplicationResult.AutoRejected, appResult);
+
+        }
+
+        [Test]
+        public void Application_SouldTransferredToAutoAccepted_WithTechStackOver75P()
+        {
+            //Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant()
+                {
+                    Age = 19
+                },
+                TechStackList = new System.Collections.Generic.List<string>()
+                {
+                    "C#", "RabbitMQ", "Microservice", "Visual Studio"
+                },
+                YearsOfExperience = 15,
+             
+
+            };
+
+            //Action
+            var appResult = evaluator.Evaluate(form);
+
+            //Assert
+            Assert.AreEqual(ApplicationResult.AutoAccepted, appResult);
 
         }
 
--- NUnitTestDemo/ApplicationEvaluator.cs
+++ NUnitTestDemo/ApplicationEvaluator.cs
@@ -11,16 +11,35 @@ namespace NUnitTestDemo
     public class ApplicationEvaluator
     {
         private const int minAge = 18;
+        private const int autoAcceptedYearsOfExperience = 10;
+        private List<string> techStackList = new() {"C#",  "RabbitMQ","Microservice", "Visual Studio"};
         public ApplicationResult Evaluate(JobApplication form)
         {
             
             if (form.Applicant.Age < minAge)
                 return ApplicationResult.AutoRejected;
 
+            var simRate = GetTechStackSimilarityRate(form.TechStackList);
+            if(simRate < 25)
+                return ApplicationResult.AutoRejected;
+
+            if (simRate > 75 && form.YearsOfExperience > autoAcceptedYearsOfExperience)
+                return ApplicationResult.AutoAccepted;
+
+           
             return ApplicationResult.AutoAccepted;
+        }
+
+        private double GetTechStackSimilarityRate(List<string> techList)
+        {
+            var matchCount = techList
+                .Where(t => techStackList.Contains(t, StringComparer.OrdinalIgnoreCase))
+                .Count();
+            return (double)matchCount / techStackList.Count * 100;
         }
         
     }
+
 
     public enum ApplicationResult
     {
